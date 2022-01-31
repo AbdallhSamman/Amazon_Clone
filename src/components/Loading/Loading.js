9f425e08a29b9d2810863d22c9456af01b3df7d8
SaharZahran <Saharzahran666@yahoo.com>
2022-01-31 13:03:24 +0200

    add loading image

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return ( 
+    <div className="photo">
+      <img src='https://c.tenor.com/JDPHxCqgEd0AAAAC/amazon.gif'/>
+    </div>
+   );
+}
+
+export default Loading;
