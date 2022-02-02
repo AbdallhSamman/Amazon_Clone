49649509dd135ef544f0384fde3b68a1e69a6b33
Abdallh Samman <90176120+AbdallhSamman@users.noreply.github.com>
2022-02-02 13:37:01 +0200

    add pagnation

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Checkout.css";
 import Subtotal from "../Subtotal/Subtotal";
 import CheckoutProduct from "../CheckoutProduct/CheckoutProduct";
 import { useStateValue } from "../../StateProvider";
+import ReactPaginate from "react-paginate";
 
 function Checkout() {
-  const [{ basket , user},dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const productsPerPage = 2;
+  const pagesVisited = pageNumber * productsPerPage;
+
+  const displayProducts = JSON.parse(localStorage.getItem('basket'))
+    .slice(pagesVisited, pagesVisited + productsPerPage)
+    .map((item) => (
+      <CheckoutProduct
+        key={item.id + Math.floor(Math.random() * 50)}
+        id={item.id}
+        title={item.title}
+        image={item.image}
+        price={item.price}
+        rating={item.rating}
+      />
+    ));
+  const pageCount = Math.ceil(basket.length / productsPerPage);
+  const changePage = ({selected})=>{
+    setPageNumber(selected);
+  }
   return (
     <div className="checkout">
       {JSON.parse(localStorage.getItem('basket')).length == 0 ? (
@@ -18,16 +40,18 @@ function Checkout() {
         <>
       <div className="checkout__left">
           <h2 className="checkout__title">Your Shopping Basket</h2>
-          {JSON.parse(localStorage.getItem('basket')).map((item) => (
-            <CheckoutProduct
-            key={item.id+Math.floor(Math.random()*50)}
-              id={item.id}
-              title={item.title}
-              image={item.image}
-              price={item.price}
-              rating={item.rating}
+           {displayProducts}
+            <ReactPaginate 
+            previousLabel={'Previous'}
+            nextLabel={'Next'}
+            pageCount={pageCount}
+            onPageChange={changePage}
+            containerClassName="paginitionBtns"
+            previousLinkClassName="previousBtns"
+            nextLinkClassName="nextBtns"
+            disabledLinkClassName="paginationDisabled"
+            activeClassName="paginationActive"
             />
-          ))}
        </div>
 
         <div className="checkout__right">
